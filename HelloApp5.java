235d3e32032b8e05f4de750ec8e0c6ef7a37014b
NotAnuj <chanuj044@gmail.com>
2026-03-25 08:27:51 +0530

    Create HelloApp5.java

---
+++ HelloApp5.java
@@ -0,0 +1,17 @@
+public class HelloApp {
+    public static void main(String[] args) {
+
+        StringBuilder namesBuilder = new StringBuilder();
+        if (args.length == 0) {
+            namesBuilder.append("World");
+        } else {
+            for (String name : args) {
+                if (namesBuilder.length() > 0) {
+                    namesBuilder.append(", ");
+                }
+                namesBuilder.append(name);
+            }
+        }
+        System.out.println("Hello, " + namesBuilder.toString() + "!");
+    }
+}
